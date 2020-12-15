cf290c8b90b18921b43da8decb6e7867366989ea
blumenthaler <blumenthaleralex@gmail.com>
2020-12-15 12:04:35 -0500

    added comment model, seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_153600) do
+ActiveRecord::Schema.define(version: 2020_12_15_170133) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "Alex the Bartender", password: "password")
+alex = User.create(username: "Alex the Bartender", password: "password")
+
+comment = Comment.create(content: "this is delicious", user: alex)
