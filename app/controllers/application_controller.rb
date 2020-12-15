eec308c6e1bd5f193b459ccce83c0a5d287a8dfc
blumenthaler <blumenthaleralex@gmail.com>
2020-12-15 15:25:41 -0500

    removed wrong helper syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::API
-    helpers do
         # first seed user is "logged in"
-        def current_user
-            User.first
-        end
+    def current_user
+        User.first
+    end
 
-        def logged_in?
-            !!current_user
-        end
+    def logged_in?
+        !!current_user
     end
 end
