42d6add1658305847e93468f2257e1c0ba7bf6ab
blumenthaler <blumenthaleralex@gmail.com>
2020-12-16 17:05:00 -0500

    configured login/authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::API
+    include ::ActionController::Cookies
         # first seed user is "logged in"
     def current_user
-        User.first
+        User.find_by(id: session[:user_id])
     end
 
     def logged_in?
