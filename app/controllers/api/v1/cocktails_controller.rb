c6dbbe90f2ef2ce91340fd4f1c24a4ca7efdf21b
blumenthaler <blumenthaleralex@gmail.com>
2020-12-17 15:54:39 -0500

    updated serialized json for cocktails#index

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::CocktailsController < ApplicationController
-    def index
-        @cocktails = Cocktail.all
-    
-        render json: @cocktails
-      end
+  def index
+    cocktails = Cocktail.all
+    options = {
+        include: [:recipes]
+    }
+    render json: CocktailSerializer.new(cocktails, options)
+  end
 end
