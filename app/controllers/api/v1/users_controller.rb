53cbd9fe36048e6b13e976e5716340e2618e1ab8
blumenthaler <blumenthaleralex@gmail.com>
2020-12-17 13:49:21 -0500

    removed byebug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,7 +16,6 @@ class Api::V1::UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-    byebug
 
     if @user.save
       render json: @user, status: :created
