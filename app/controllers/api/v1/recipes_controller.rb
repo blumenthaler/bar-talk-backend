aa5304cf523b9983c80762d072a311ba2d902bb4
blumenthaler <blumenthaleralex@gmail.com>
2020-12-29 16:04:51 -0500

    added create recipe

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -6,4 +6,23 @@ class Api::V1::RecipesController < ApplicationController
         }
         render json: RecipeSerializer.new(recipes, options)
     end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+        @recipe.votes = 0
+        options = {
+            include: [:user, :cocktail, :comments]
+        }
+        if @recipe.save
+            render json: RecipeSerializer.new(@recipe, options)
+        else
+            render json: @recipe.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :spirit, :ingredients, :garnish, :notes, :user_id, :cocktail_id)
+    end
 end
