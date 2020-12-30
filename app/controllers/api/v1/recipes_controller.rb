0210aefeb1382b0ff03c69292a3782ef9365b006
blumenthaler <blumenthaleralex@gmail.com>
2020-12-30 15:37:19 -0500

    added find or create by functionality for recipes#create

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -8,15 +8,29 @@ class Api::V1::RecipesController < ApplicationController
     end
 
     def create
-        @recipe = Recipe.new(recipe_params)
-        @recipe.votes = 0
-        options = {
-            include: [:user, :cocktail, :comments]
-        }
-        if @recipe.save
-            render json: RecipeSerializer.new(@recipe, options)
+        recipe = Recipe.new(recipe_params)
+        recipe.votes = 0
+        # find all cocktails with the recipe's name
+        named = Cocktail.all.select{|c| c.name === recipe_params[:name]}
+
+        spirit = recipe_params[:spirit].downcase
+
+        # try to find the cocktail with same name AND spirit
+        maybe_cocktail = named.find{|c| c.spirit === spirit}
+        if maybe_cocktail
+            recipe.cocktail = maybe_cocktail
         else
-            render json: @recipe.errors, status: :unprocessable_entity
+            # if cocktail of same name and spirit does not exist, create one!
+            new_cocktail = Cocktail.create(name: recipe_params[:name], spirit: spirit)
+            recipe.cocktail = new_cocktail
+        end 
+        if recipe.save
+            options = {
+                include: [:user, :cocktail, :comments]
+            }
+            render json: RecipeSerializer.new(recipe, options)
+        else
+            render json: recipe.errors, status: :unprocessable_entity
         end
     end
 
