bfd308e4f9eff76a2080100849313469556bea26
blumenthaler <blumenthaleralex@gmail.com>
2021-01-04 16:53:56 -0500

    created comments#create action

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,4 +6,23 @@ class Api::V1::CommentsController < ApplicationController
         }
         render json: CommentSerializer.new(comments, options)
     end
+
+    def create
+        comment = Comment.new(comment_params)
+        if comment.save
+            options = {
+                include: [:user, :recipe]
+            }
+            render json: CommentSerializer.new(comment, options)
+        else
+            render json: comment.errors, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :user_id, :recipe_id)
+    end
 end
