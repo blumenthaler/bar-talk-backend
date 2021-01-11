7f78a6825114c229cc74a08236c4457fbaa6bdc1
blumenthaler <blumenthaleralex@gmail.com>
2021-01-11 14:21:36 -0500

    UsersController: removed unused controller actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
-
   # GET /users
   def index
     users = User.all
@@ -8,11 +6,6 @@ class Api::V1::UsersController < ApplicationController
             include: [:recipes]
         }
         render json: UserSerializer.new(users, options)
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
   end
 
   # POST /users
@@ -26,25 +19,7 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
     # Only allow a trusted parameter "white list" through.
     def user_params
